7dae2b0c60c9239e03fb699b3b65a4d2470259e8
Grzegorz Mrukwa <g.mrukwa@gmail.com>
2018-01-06 14:51:00 +0100

    Add 3D coordinates to types module

--- spdata/types.py
+++ spdata/types.py
@@ -1,1 +1,33 @@
 """Reusable types across package"""
+
+
+import numpy as np
+
+
+class Coordinates:
+    """X, Y, Z coordinates"""
+    def __init__(self, x, y, z):
+        """
+        Args:
+            x: x-coordinates for each spectrum
+            y: y-coordinates for each spectrum
+            z: z-coordinates for each spectrum
+
+        Raises:
+            ValueError
+        """
+        self.x = np.array(x)
+        self.y = np.array(y)
+        self.z = np.array(z)
+        self._validate()
+
+    def __len__(self):
+        return self.x.size
+
+    def _validate(self):
+        if self.x.size != self.y.size:
+            raise ValueError("Number of X and Y coordinates should be equal. "
+                             "Was: %i and %i" % (self.x.size, self.y.size))
+        if self.x.size != self.z.size:
+            raise ValueError("Number of X and Z coordinates should be equal. "
+                             "Was: %i and %i" % (self.x.size, self.z.size))
--- test/test_types.py
+++ test/test_types.py
@@ -1,3 +1,13 @@
 import unittest
 
 import spdata.types as ty
+
+
+class TestCoordinates(unittest.TestCase):
+    def test_throws_on_different_size_coordinates(self):
+        with self.assertRaises(ValueError):
+            ty.Coordinates(x=[1, 2], y=[3], z=[4, 5])
+        with self.assertRaises(ValueError):
+            ty.Coordinates(x=[1, 2], y=[3, 4], z=[5])
+        with self.assertRaises(ValueError):
+            ty.Coordinates(x=[1], y=[2, 3], z=[4, 5])
