fc65f099c2694d6772a3da1492e01a3e4d6ef929
Grzegorz Mrukwa <g.mrukwa@gmail.com>
2018-01-06 14:51:18 +0100

    Add simplistic Dataset class to types module

--- spdata/types.py
+++ spdata/types.py
@@ -31,3 +31,40 @@ class Coordinates:
         if self.x.size != self.z.size:
             raise ValueError("Number of X and Z coordinates should be equal. "
                              "Was: %i and %i" % (self.x.size, self.z.size))
+
+
+class Dataset:
+    """Simplistic common interface for MSI data"""
+    # @gmrukwa: types purposefully left blank to preserve flexibility
+    def __init__(self, spectra, coordinates: Coordinates, mz, labels=None):
+        """
+        Args:
+            spectra: measured values of spectra with spectra in rows and mass
+            channels in columns
+            coordinates (Coordinates): coordinates for each spectrum
+            mz: values of m/z for mass channels
+            labels: optional labels for spectra
+
+        Raises:
+            ValueError
+        """
+        self.spectra = np.array(spectra)
+        self.coordinates = coordinates
+        self.mz = np.array(mz)
+        self.labels = np.array(labels) if labels is not None else None
+        self._validate()
+
+    def _validate(self):
+        if self.labels is not None and self.labels.size != len(
+                self.coordinates):
+            raise ValueError("Number of labels and coordinates should be equal."
+                             " Was: %i and %i"
+                             % (self.labels.size, len(self.coordinates)))
+        if self.spectra.shape[0] != len(self.coordinates):
+            raise ValueError("Number of spectra should be equal number of "
+                             "coordinates. Were: %i and %i"
+                             % (self.spectra.shape[0], len(self.coordinates)))
+        if self.spectra.shape[1] != self.mz.size:
+            raise ValueError("Number of features spectra should be equal "
+                             "number of m/z-s registered. Were: %i and %i"
+                             % (self.spectra.shape[1], self.mz.size))
