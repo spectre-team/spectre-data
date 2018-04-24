97562eed40e046a9601ea2efdc22efb847280312
Daroslaw <daroslawowicz@gmail.com>
2018-04-24 18:18:55 +0200

    Used full name for an import.

--- test/test_discover.py
+++ test/test_discover.py
@@ -21,7 +21,7 @@ from unittest.mock import MagicMock
 
 from typing import List, Dict
 
-import spdata.discover as disc
+from spdata import discover
 from spdata.common import Path, Name
 
 test_datasets = [
@@ -40,28 +40,29 @@ class TestNameToId(unittest.TestCase):
     @patch('spdata.discover.get_datasets')
     def test_generates_some_unique_ids(self, mock):
         mock.return_value = test_datasets
-        ids = {disc.name_to_id(value) for _, value in enumerate(d['name'] for d in disc.get_datasets())}
+        ids = {discover.name_to_id(value) for _, value in enumerate(d['name'] 
+            for d in discover.get_datasets())}
         self.assertEqual(len(ids), len(test_datasets))
 
     def test_id_is_consistent_for_the_same_name(self):
-        self.assertEqual(disc.name_to_id("name"), disc.name_to_id("name"))
+        self.assertEqual(discover.name_to_id("name"), discover.name_to_id("name"))
 
 class TestIdToName(unittest.TestCase):
     @patch('spdata.discover.get_datasets')
     def test_finds_name_by_its_id(self, mock):
         mock.return_value = test_datasets
         some_name = test_datasets[1]['name']
-        some_id = disc.name_to_id(some_name)
+        some_id = discover.name_to_id(some_name)
         self.assertEqual(
-            disc.id_to_name(some_id),
+            discover.id_to_name(some_id),
             some_name
         )
 
     @patch('spdata.discover.get_datasets')
     def test_throws_for_nonexistent_name(self, mock):
         mock.return_value = test_datasets
-        with self.assertRaises(disc.UnknownIdError):
-            disc.id_to_name(123)
+        with self.assertRaises(discover.UnknownIdError):
+            discover.id_to_name(123)
 
 class TestGetDatasets(unittest.TestCase):
     @patch('os.listdir')
@@ -69,7 +70,7 @@ class TestGetDatasets(unittest.TestCase):
     def test_returns_correct_datasets_set(self, mock_isdir, mock_listdir):
         mock_isdir.return_value = True
         mock_listdir.return_value = test_listdir 
-        self.assertEqual(disc.get_datasets(), test_datasets)
+        self.assertEqual(discover.get_datasets(), test_datasets)
 
 class TestDatasetExists(unittest.TestCase):
     @patch('os.listdir')
@@ -77,11 +78,11 @@ class TestDatasetExists(unittest.TestCase):
     def test_returns_true_for_existing_dataset(self, mock_isdir, mock_listdir):
         mock_isdir.return_value = True
         mock_listdir.return_value = test_listdir 
-        self.assertTrue(disc.dataset_exists("dataset number one"))
+        self.assertTrue(discover.dataset_exists("dataset number one"))
 
     @patch('os.listdir')
     @patch('os.path.isdir')
     def test_returns_false_for_nonexisting_dataset(self, mock_isdir, mock_listdir):
         mock_isdir.return_value = True
         mock_listdir.return_value = test_listdir 
-        self.assertFalse(disc.dataset_exists("dataset number four"))
+        self.assertFalse(discover.dataset_exists("dataset number four"))
